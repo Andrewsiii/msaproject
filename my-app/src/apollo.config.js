82262f00746930b16c1a2760578f0040de61d4ef
asio896 <68887738+asio896@users.noreply.github.com>
2021-09-11 20:16:54 +1200

    changed to devop

--- my-app/src/apollo.config.js
+++ my-app/src/apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
     client: {
       service: {
         name: "my-app",
-        url: "http://localhost:2522/graphql",
+        url: "https://msa-proj.azurewebsites.net/graphql/",
       },
     },
   };
